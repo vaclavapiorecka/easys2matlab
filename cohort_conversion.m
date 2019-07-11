4a186c885d7d4a7d23e3556cff44d6a7550d3f52
vaclavapiorecka <vaclava.piorecka@gmail.com>
2019-07-11 13:20:27 +0200

    Fix error (longterm data), Add cohort conversion

---
+++ cohort_conversion.m
@@ -0,0 +1,34 @@
+% This set of functions was created based on the reference manual to the
+% EASYS2 file format: EASYS2 Reference Manual. 3rd edition. Praha: Neuroscience Technology Research, 2002.
+%
+% AUTHOR:   Vaclava Piorecka
+% CONTACT:  vaclava.piorecka@fbmi.cvut.cz, vaclava.piorecka@nudz.cz
+% DATE:     2018/08/28
+
+clc; clear; close all;
+
+[fileNames,pathName] = uigetfile({'*.d','EASYS2 (*.d)'; ...
+                '*.*',  'All Files (*.*)'}, ...
+                'Select a File',...
+                'MultiSelect', 'on');
+
+numOfFiles = size(fileNames,2);
+
+fwait = waitbar(0,'Please wait...');
+
+for nof = 1 : 1 : numOfFiles
+    fullPath = fullfile(pathName,fileNames{1,nof});
+    
+    standardHeader  = readSHeaderEASYS2(fullPath);                  % Get stnadard header
+    extendedHeader  = readXHeaderEASYS2(fullPath,standardHeader );  % Get extended header
+    
+    % Get data
+    dataCalib       = readDataEASYS2(fullPath, standardHeader, extendedHeader);
+    
+    % Conversion of the data structure to the EEGLAB / FieldTrip.
+    dataEEGLAB      = easys2eeglab( fullPath, standardHeader, extendedHeader, dataCalib );
+    dataFieldtrip   = easys2fieldtrip( fullPath, standardHeader, extendedHeader, dataCalib );
+    
+    save([pathName,fileNames{1,nof}(1:end-1),'.mat'],'dataFieldtrip','-v7.3');
+    waitbar(nof / numOfFiles)
+end
